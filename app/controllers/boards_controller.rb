7a3dae42d3d64fedd6e7bb6a4f8957ed99f0ce4b
Gustavo Joshua Gutierrez <51761125+evolshenanigans@users.noreply.github.com>
2024-02-21 21:31:00 +0000

    finshed the tabs

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,6 +3,7 @@ class BoardsController < ApplicationController
     matching_boards = Board.all
 
     @list_of_boards = matching_boards.order({ :created_at => :desc })
+    
 
     render({ :template => "boards/index" })
   end
